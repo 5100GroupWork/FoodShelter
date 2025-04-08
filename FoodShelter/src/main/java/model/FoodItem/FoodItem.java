c3e8e90128498ab15977943ef2f2f5f1462167d8
Yige Wang <wang.yige@northeastern.edu>
2025-04-08 14:45:24 -0400

    setting model.FoodCatalog

---
+++ FoodShelter/src/main/java/model/FoodItem/FoodItem.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.FoodItem;
+
+/**
+ *
+ * @author 59386
+ * this is for food Inc employee to post food
+ */
+public class FoodItem {
+    String FoodName;
+//    String FoodCata
+}
