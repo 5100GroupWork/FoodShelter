dbbb7784f20d478fe2324cdff6d4ead2d3d363a6
Christiana-YueW <wu.y3@northeastern.edu>
2025-04-18 22:44:13 -0400

    Yuewu debug (#58)
    * fix task manager
    
    * button implementation
    
    * update driver work panel
    
    * food enterprise initiation
    
    * new employee arraylist
    
    * fix freshcheck and foodshleter config

--- FoodShelter/src/main/java/model/Organization/FoodIncOrg.java
+++ FoodShelter/src/main/java/model/Organization/FoodIncOrg.java
@@ -37,6 +37,7 @@ public class FoodIncOrg extends BasicOrganization{
     }
     public FoodIncOrg(String name) {
         super(Type.FoodInc.getValue(),name);
+        this.employees = new ArrayList<>();
     }
     
     @Override
