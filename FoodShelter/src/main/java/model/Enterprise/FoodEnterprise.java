99c138bd245088098b51ac1c1e3cceb2eac43b01
Yue Wu <wu.y3@northeastern.edu>
2025-04-18 22:19:10 -0400

    food enterprise initiation

--- FoodShelter/src/main/java/model/Enterprise/FoodEnterprise.java
+++ FoodShelter/src/main/java/model/Enterprise/FoodEnterprise.java
@@ -26,6 +26,9 @@ public class FoodEnterprise extends BasicEnterprise{
     // 导入需要的名称，比如xxx食品公司
     public FoodEnterprise(String name){
         super(name, BasicEnterpriseType.Food);
+        this.foodIncOrgs = new ArrayList<>();
+        this.employees = new ArrayList<>();
+        
     }
     // add
     public FoodIncOrg addFoodIncOrg(String name){
