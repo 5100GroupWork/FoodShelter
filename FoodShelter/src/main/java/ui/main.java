dd7fb4e3b35ab00ee0ed1c05be8e4585117e0f34
Yue Wu <wu.y3@northeastern.edu>
2025-04-20 21:44:35 -0400

    page flesh

--- FoodShelter/src/main/java/ui/main.java
+++ FoodShelter/src/main/java/ui/main.java
@@ -242,7 +242,11 @@ public class main extends javax.swing.JFrame {
                     inNetwork, system);
                 container.add("workArea", workArea);
                 CardLayout layout = (CardLayout) container.getLayout();
+                System.out.println("before jump");
                 layout.show(container, "workArea");
+                System.out.println("after jump");
+                container.revalidate();
+                container.repaint();
 
                 // 状态更新
                 loginJButton.setEnabled(false);
@@ -318,6 +322,8 @@ public class main extends javax.swing.JFrame {
         container.add("workArea", workArea);
         CardLayout layout = (CardLayout) container.getLayout();
         layout.show(container, "workArea");
+        container.revalidate();
+        container.repaint();
 
         // 状态更新
         loginJButton.setEnabled(false);
